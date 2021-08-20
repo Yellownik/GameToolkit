c9b2d0b2abb75bc21d514508c570b7df943d51bd
Nikolay <nmaschinskiy@mail.ru>
2021-08-21 00:21:26 +0300

    added utils for Promise, Animator and RectTransform

---
+++ Assets/Scripts/Utils/PromiseUtils.cs
@@ -0,0 +1,36 @@
+﻿//using RSG;
+using Orbox.Async;
+
+namespace Utils
+{
+    public static class PromiseUtils
+    {
+        //public static void RecreateAndResolve(ref Promise promise)
+        //{
+        //    var oldPromise = promise;
+        //    promise = new Promise();
+        //    oldPromise.Resolve();
+        //}
+
+        //public static void RecreateAndResolve<T>(ref Promise<T> promise, T t)
+        //{
+        //    var oldPromise = promise;
+        //    promise = new Promise<T>();
+        //    oldPromise.Resolve(t);
+        //}
+
+        public static void RecreateAndResolve(ref Deferred promise)
+        {
+            var oldPromise = promise;
+            promise = new Deferred();
+            oldPromise.Resolve();
+        }
+
+        public static void RecreateAndResolve<T>(ref Deferred<T> promise, T t)
+        {
+            var oldPromise = promise;
+            promise = new Deferred<T>();
+            oldPromise.Resolve(t);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/RectTransformUtils.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class RectTransformUtils
+    {
+        public static void StretchRectTransform(RectTransform transform)
+        {
+            transform.anchoredPosition = transform.parent.position;
+            transform.anchorMin = new Vector2(0f, 0f);
+            transform.anchorMax = new Vector2(1f, 1f);
+            transform.offsetMin = Vector2.zero;
+            transform.offsetMax = Vector2.zero;
+            transform.pivot = new Vector2(0.5f, 0.5f);
+        }
+        
+        public static void TopStretchRectTransform(RectTransform transform)
+        {
+            transform.anchoredPosition = transform.parent.position;
+            transform.anchorMin = new Vector2(0f, 1f);
+            transform.anchorMax = new Vector2(1f, 1f);
+            transform.offsetMin = Vector2.zero;
+            transform.offsetMax = Vector2.zero;
+            transform.pivot = new Vector2(0.5f, 1f);
+        }
+        
+        public static void StickRectTransformToTop(RectTransform transform)
+        {
+            transform.anchoredPosition = transform.parent.position;
+            transform.anchorMin = new Vector2(0.5f, 1f);
+            transform.anchorMax = new Vector2(0.5f, 1f);
+            transform.anchoredPosition = Vector2.zero;
+            transform.pivot = new Vector2(0.5f, 1f);
+        }
+    }
+}
