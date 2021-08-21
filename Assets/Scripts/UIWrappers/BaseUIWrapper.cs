0f86444a6a9c25e4896f78c02ad7d02aa32801a6
Nikolay <nmaschinskiy@mail.ru>
2021-08-21 19:19:16 +0300

    created BaseUIWrapper with Show/Hide methods

---
+++ Assets/Scripts/UIWrappers/BaseUIWrapper.cs
@@ -0,0 +1,22 @@
+﻿using Orbox.Async;
+using UnityEngine;
+using Utils;
+
+namespace UIWrappers
+{
+    public abstract class BaseUIWrapper : MonoBehaviour
+    {
+        public abstract IPromise Show();
+        public abstract IPromise Hide();
+
+        protected IPromise BaseScale(bool isShow)
+        {
+            var promise = new Deferred();
+
+            transform.DoScale(isShow)
+                .onComplete += () => promise.Resolve();
+
+            return promise;
+        }
+    }
+}
