a1a6da1811c5167ac22154be37c5fe6c099ee67f
Nikolay <nmaschinskiy@mail.ru>
2021-08-31 11:18:28 +0300

    created GameStarter.cs | added GameStarter, Root and EventSystem on SampleScene

---
+++ Assets/Scripts/GameManagers/GameStarter.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using UnityEngine;
+
+namespace GameManagers
+{
+    public class GameStarter : MonoBehaviour
+    {
+        void Start()
+        {
+            Root.TimerService.Wait(0.01f)
+                .Done(() => Root.GameManager.Run());
+        }
+    }
+}
