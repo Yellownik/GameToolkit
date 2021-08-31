799ca05121b31add6741969c63f16e07305ef776
Nikolay <nmaschinskiy@mail.ru>
2021-08-31 23:19:44 +0300

    created DemoLevel

---
+++ Assets/Scripts/GameManagers/GameLevels/DemoLevel.cs
@@ -0,0 +1,25 @@
+﻿using ButtonListeners;
+using Core;
+using UnityEngine;
+
+namespace GameManagers
+{
+    public class DemoLevel : MonoBehaviour
+    {
+        [SerializeField] private ButtonListener Button;
+        [SerializeField] private int ClicksToEnd = 10;
+        [SerializeField] private float RandomRange = 100;
+        [SerializeField] private bool IsUniqueFlyText = true;
+
+        void Start()
+        {
+            Button.AddFunction(OnClick);
+        }
+
+        private void OnClick()
+        {
+            Root.FlyTextManager.Spawn(Button.transform, ClicksToEnd.ToString(), Random.insideUnitCircle * RandomRange, isUnique: IsUniqueFlyText);
+            ClicksToEnd--;
+        }
+    }
+}
