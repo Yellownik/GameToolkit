564dc0e702b0689e2a8f1928f1641a8ca8e65ce4
Nikolay <nmaschinskiy@mail.ru>
2025-06-03 22:25:46 +0300

    fixed build error it EditorUtils.cs

--- Assets/Scripts/EditorUtils/EditorUtils.cs
+++ Assets/Scripts/EditorUtils/EditorUtils.cs
@@ -1,15 +1,20 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace EditorUtils
 {
     public class EditorUtils
     {
+	    #if UNITY_EDITOR
 		public static void DrawScriptHeader<T>(T testUtils) where T : MonoBehaviour
 		{
 			GUI.enabled = false;
 			EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour(testUtils), testUtils.GetType(), false);
 			GUI.enabled = true;
 		}
+		#endif
 	}
 }
